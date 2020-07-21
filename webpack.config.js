3559b9db442d88081ec31913c25111fc2df20d1c
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-07-21 15:00:49 +0300

    add webpack plugins

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');  // require html-webpack-plugin for html template
+const {CleanWebpackPlugin} = require('clean-webpack-plugin'); // clean old files from output path
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.[chunkhash].js',          // add hash to bundle files
+        path: path.resolve(__dirname, 'public')
+    },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+        new HTMLPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ]
+}
